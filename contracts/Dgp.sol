b7738ddd14b29567475126c5f9b7d484428a13db
Charlie Knoll <charlie.knoll@gmail.com>
2017-08-07 20:48:42 -0600

    Data type changes to reduce gas deployment costs

--- contracts/Dgp.sol
+++ contracts/Dgp.sol
@@ -4,44 +4,45 @@ pragma solidity ^0.4.11;
 contract Dgp {
 
     struct Client {
-        uint256 startTime;
-        uint256 lastRedemptionBlock;
-        uint256 checkingBalance;
-        uint256 depositedEndowments;
-        uint256 endowmentTotal;
+        uint startTime;
+        uint lastRedemptionBlock;
+        uint32 checkingBalance;
+        uint32 depositedEndowments;
+        uint32 endowmentTotal;
     }    
     struct Vendor {
         uint8 registered;
-        uint256 balance;
+        uint32 balance;
     }
     address public superAdmin;
     address public admin;
-    uint256 public accountBalance;
-    uint256 public allocated;
-    uint256 public constant redemptionFrequency = 7; //7 days
-    uint256 public constant redemptionAmt = 7000; //DUST (in cents)
-    //uint256 public minClientBalance =    3000000000000000; //wei .003 ETH use 2500000000000000 for testing due to truffle gas bug
-    // uint256 public clientGasAddAmt =  15000000000000000; //wei .015 ETH PRODUCTION
-    uint256 public minClientBalance =   97000000000000000; //wei .0970 ETH use 2500000000000000 for testing due to truffle gas bug
-    uint256 public clientGasAddAmt =    100000000000000000; //wei .1 ETH TESTING due to truffle gas bug, just enough for 2 tx's causing a top off before 3rd
+    uint32 public accountBalance;
+    uint public allocated;
+    uint32 public constant redemptionFrequency = 7; //7 days
+    uint32 public constant redemptionAmt = 7000; //DUST (in cents)
+    uint public minClientBalance =    3000000000000000; //wei .003 ETH use 2500000000000000 for testing due to truffle gas bug
+    uint public clientGasAddAmt =  15000000000000000; //wei .015 ETH PRODUCTION
+    // uint public minClientBalance =   97000000000000000; //wei .0970 ETH use 2500000000000000 for testing due to truffle gas bug
+    // uint public clientGasAddAmt =    100000000000000000; //wei .1 ETH TESTING due to truffle gas bug, just enough for 2 tx's causing a top off before 3rd
 
     mapping (address => Vendor) public vendors;
     mapping (address => Client) public clients;
-    mapping (address => uint256) public supporters;
+    mapping (address => uint) public supporters;
 
 
 
     // events
-    event USDDonation(uint256 _value);
-    event UnlockedDeposit(address indexed _to, uint256 _value);
-    event LockedDeposit(address indexed _to, uint256 _value);
-    event RemoveClient(address indexed _removed, uint256 _value);
-    event Purchase(address indexed _client, address indexed _vendor, uint256 _value);
-    event Refund(address indexed _vendor, address _client, uint256 _value);
-    event Redemption(address indexed _vendor, uint256 _value);
-    event SupporterETHDonation(address indexed _supporter, uint256 _value);
+    event USDDonation(uint32 _value);
+    event UnlockedDeposit(address indexed _to, uint32 _value);
+    event LockedDeposit(address indexed _to, uint32 _value);
+    event RemoveClient(address indexed _removed, uint32 _value);
+    event Purchase(address indexed _client, address indexed _vendor, uint32 _value);
+    event Refund(address indexed _vendor, address _client, uint32 _value);
+    event Redemption(address indexed _vendor, uint32 _value);
+    event PurchaseDonation(address indexed _vendor, uint32 _value);
+    event SupporterETHDonation(address indexed _supporter, uint _value);
     event RunningOutOfGas(address indexed _client);
-    event Transfer(address _recipient, uint256 _value);
+    event Transfer(address _recipient, uint _value);
     
     //access control
     modifier onlySuperAdmin { 
@@ -73,12 +74,12 @@ contract Dgp {
       superAdmin = msg.sender;
       admin = _admin;
     }
-    function registerDonation(uint256 _donationAmt) onlyAdmin() {
+    function registerDonation(uint32 _donationAmt) onlyAdmin() {
         accountBalance += _donationAmt;
         USDDonation(_donationAmt);
     }
     
-    function registerClient(address _clientAddress, uint256 _endowmentAmt, uint256 _startTime) onlyAdmin() {
+    function registerClient(address _clientAddress, uint32 _endowmentAmt, uint _startTime) onlyAdmin() {
         //never allocate more than account balance
         if (_endowmentAmt <= 0) revert();
         if (_endowmentAmt + allocated > accountBalance) revert();
@@ -107,14 +108,14 @@ contract Dgp {
     } 
 
     //Used by admin to give immediately vested DUST to client
-    function depositChecking(address _clientAddress, uint256 _amount) onlyAdmin() external {
+    function depositChecking(address _clientAddress, uint32 _amount) onlyAdmin() external {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
         if (allocated + _amount > accountBalance) revert();
         allocated += _amount;
         clients[_clientAddress].checkingBalance += _amount;
         UnlockedDeposit(_clientAddress, _amount);
     }
-    function depositSavings(address _clientAddress, uint256 _amount) onlyAdmin() external {
+    function depositSavings(address _clientAddress, uint32 _amount) onlyAdmin() external {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
         if (allocated + _amount > accountBalance) revert();
         allocated += _amount;
@@ -123,34 +124,34 @@ contract Dgp {
     }
     function removeClient(address _clientAddress) onlyAdmin() external {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
-        uint256 clientFunds = clients[_clientAddress].checkingBalance +
+        uint32 clientFunds = clients[_clientAddress].checkingBalance +
          clients[_clientAddress].endowmentTotal - clients[_clientAddress].depositedEndowments;
          
         allocated -= clientFunds;
         RemoveClient(_clientAddress, clientFunds);
         delete clients[_clientAddress];
     }
-    function getVested(address _clientAddress) constant returns(uint256 vested) {
+    function getVested(address _clientAddress) constant returns(uint32 vested) {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
-        uint256 endowmentDuration = (now - clients[_clientAddress].startTime);
-        uint256 earnedEndowments = endowmentDuration / (redemptionFrequency * 1 days);
+        uint endowmentDuration = (now - clients[_clientAddress].startTime);
+        uint32 earnedEndowments = uint32(endowmentDuration / (redemptionFrequency * 1 days));
         return (earnedEndowments * redemptionAmt - clients[_clientAddress].depositedEndowments);
     }
-    function getCheckingBalance(address _clientAddress) constant external returns(uint256 checkingBalance) {
+    function getCheckingBalance(address _clientAddress) constant external returns(uint32 checkingBalance) {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
         checkingBalance = getVested(_clientAddress) + clients[_clientAddress].checkingBalance;
     }
 
     //Returns a virtual savings account balance, i.e. that which has been endowed but that can't be spent, maybe call it TrustBalance?
-    function getSavingsBalance(address _clientAddress) constant external returns(uint256 savingsBalance) {
+    function getSavingsBalance(address _clientAddress) constant external returns(uint32 savingsBalance) {
         if (clients[_clientAddress].endowmentTotal == 0) revert();
         savingsBalance = clients[_clientAddress].endowmentTotal -
         getVested(_clientAddress) - clients[_clientAddress].depositedEndowments;
     }
 
-    function makePurchase(address _vendorAddress, uint256 amount) onlyClient() external  {
+    function makePurchase(address _vendorAddress, uint32 amount) onlyClient() external  {
         if (vendors[_vendorAddress].registered == 0) revert(); //vendor check
-        uint256 vested = getVested(msg.sender);
+        uint32 vested = getVested(msg.sender);
         if (vested + clients[msg.sender].checkingBalance < amount) revert();
         clients[msg.sender].depositedEndowments += vested;
         clients[msg.sender].checkingBalance += vested-amount;
@@ -158,17 +159,17 @@ contract Dgp {
         checkClientFunds(msg.sender);
         Purchase(msg.sender, _vendorAddress, amount);
     }
-    function refundClient(address _clientAddress, uint256 amount) onlyVendorWithBalance() external {
+    function refundClient(address _clientAddress, uint32 amount) onlyVendorWithBalance() external {
         if (clients[_clientAddress].endowmentTotal == 0) revert(); //client addr check
         if (vendors[msg.sender].balance < amount) revert();
         clients[_clientAddress].checkingBalance += amount;
         Refund(msg.sender,_clientAddress,amount);
     }
 
-    function makePurchaseForClient(address _vendorAddress, address _clientAddress, uint256 amount)
+    function makePurchaseForClient(address _vendorAddress, address _clientAddress, uint32 amount)
        onlyAdmin() external {
         if (vendors[_vendorAddress].registered == 0) revert(); //vendor check
-        uint256 vested = getVested(_clientAddress);
+        uint32 vested = getVested(_clientAddress);
         if (vested + clients[_clientAddress].checkingBalance < amount) revert();
         clients[_clientAddress].depositedEndowments += vested;
         clients[_clientAddress].checkingBalance += vested-amount;
@@ -177,22 +178,28 @@ contract Dgp {
     }
     
     function redeemPurchases() onlyVendorWithBalance() external {
-        uint256 balance = vendors[msg.sender].balance;
+        uint32 balance = vendors[msg.sender].balance;
         vendors[msg.sender].balance = 0;
         accountBalance -= balance;
         allocated -= balance;
         Redemption(msg.sender,balance);
     }
-
+    function donatePurchase(uint32 _amount) onlyVendorWithBalance() external {
+        uint32 balance = vendors[msg.sender].balance;
+        if (_amount > balance) revert();
+        accountBalance -= _amount;
+        allocated -= _amount;
+        PurchaseDonation(msg.sender,_amount);
+    }
     function redeemPurchasesForVendor(address _vendorAddress) onlyAdmin() external {
         if (vendors[_vendorAddress].registered == 0) revert(); //vendor check
-        uint256 balance = vendors[_vendorAddress].balance;
+        uint32 balance = vendors[_vendorAddress].balance;
         vendors[_vendorAddress].balance = 0;
         accountBalance -= balance;
         allocated -= balance;
         Redemption(_vendorAddress,balance);
     }
-    function transferETH(address _recipient, uint256 amount) onlySuperAdmin() external {
+    function transferETH(address _recipient, uint amount) onlySuperAdmin() external {
        if (this.balance < amount) revert();
        _recipient.transfer(amount);
 
